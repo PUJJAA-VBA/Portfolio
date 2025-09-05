b2959cfe4a0ace482ba5e962d68bc440cc9c3c1e
Pujjaa VBA <pujjaabaskaran2005@gmail.com>
2025-09-05 21:38:08 +0530

    Deploy new version

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -96,7 +96,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4dc769c2b99d357e3f30b103b1a6518f
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "aa9f7b6eed2a1a78007646c867a857bd",
+"flutter_bootstrap.js": "491a14c69e874bec39770100bb8cb59c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
