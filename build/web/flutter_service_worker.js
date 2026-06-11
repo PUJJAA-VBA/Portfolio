6118fd04d297dcc31c0e63bfc7cf8f98d5c2f290
Pujjaa VBA <pujjaabaskaran2005@gmail.com>
2026-06-12 01:22:42 +0530

    Fix GitHub Pages deployment

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -40,7 +40,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "dc0e03e89ca1fe8c05938024f36950f0
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "f574f152a40b3aa89ea032e3e4731211",
+"flutter_bootstrap.js": "5ce3bf3a3fa1942ff34aa8accf38fab7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
